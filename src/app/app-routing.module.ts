b1871dbb9212a1ed239fbe1b2839dfc806829b60
hajarsalhi <shajar32@gmail.com>
2022-02-14 14:05:13 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { HomeComponent } from './home/home.component';
+import { PcAccessoiresComponent } from './pc-accessoires/pc-accessoires.component';
+import { PcBureauComponent } from './pc-bureau/pc-bureau.component';
+import { PcPortableComponent } from './pc-portable/pc-portable.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : 'Home', component : HomeComponent},
+  { path : 'PcPortable', component: PcPortableComponent},
+  { path : 'PcDeBureau', component:PcBureauComponent},
+  { path : 'Accessoires',component : PcAccessoiresComponent},
+  { path : 'LogIn', component : ConnexionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
